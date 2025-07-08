9d0607c09b07e22e0c387fa1e8fe43129931b3f1
Rakib011110 <rakib088880@gmail.com>
2025-07-08 18:32:32 +0600

    all code added

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -4,7 +4,7 @@ const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     // baseUrl: "http://localhost:5000/api",
-    baseUrl: "https://engrhachnayenahdmed-c6pbsk0ek-rakib011110s-projects.vercel.app/api",
+    baseUrl: "https://engrhachnayenahdmed-api.vercel.app/api",
   }),
   tagTypes: ["Course", "Blog", "Seminar"],
   endpoints: () => ({}),
